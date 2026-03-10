e63b1967d6f36a2d94910da58d4fdef6c0aac6ea
이재광 <yellow@wordncode.com>
2026-03-10 12:25:34 +0900

    chore: Regenerate Next.js development build artifacts and update package lock file.

---
+++ attendance-app/.next/dev/static/chunks/attendance-app_a0ff3932._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_8da675cd._.js",
+  "static/chunks/6648b_next_dist_compiled_react-dom_11813804._.js",
+  "static/chunks/6648b_next_dist_compiled_react-server-dom-turbopack_4ba1b6c6._.js",
+  "static/chunks/6648b_next_dist_compiled_next-devtools_index_d3193827.js",
+  "static/chunks/6648b_next_dist_compiled_73cd93ec._.js",
+  "static/chunks/6648b_next_dist_client_2c277e29._.js",
+  "static/chunks/6648b_next_dist_afbc3db9._.js",
+  "static/chunks/6648b_@swc_helpers_cjs_318786cd._.js"
+],
+    source: "entry"
+});
---
+++ attendance-app/.next/dev/static/chunks/attendance-app_src_app_layout_tsx_9f2ab5ba._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/attendance-app_src_app_globals_849982f9.css",
+  "static/chunks/6648b_next_dist_b74a1890._.js"
+],
+    source: "dynamic"
+});
